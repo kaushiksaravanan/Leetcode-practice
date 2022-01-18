38a9ec664c186360e6bfe591971820aee8f65b4f
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-01-19 00:54:09 +0530

    Time: 28 ms (88.60%), Space: 14.4 MB (8.41%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -11,4 +11,7 @@ def fb(n):
 
 class Solution:
     def fib(self, n: int) -> int:
-        return fb(n)
+        a=[0,1]
+        for i in range(0,n):
+            a.append(a[i]+a[i+1])
+        return a[n]
