db7d70bdd81e571890654f4e88f17444c997bafb
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-01-19 00:54:24 +0530

    Time: 51 ms (35.34%), Space: 14 MB (90.27%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,14 +1,3 @@
-a={}
-def fb(n):
-    if n not in a:
-        if n==0:
-            return 0
-        if n==1:
-            return 1
-        else:
-            a[n]=fb(n-2)+fb(n-1)
-    return a[n]
-
 class Solution:
     def fib(self, n: int) -> int:
         a=[0,1]
