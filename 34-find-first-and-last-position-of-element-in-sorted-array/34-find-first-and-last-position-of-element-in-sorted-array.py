69bca2aa03d6e6580cc88cf1e6d5b664370ed7b3
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-01-24 21:22:06 +0530

    Time: 88 ms (62.47%), Space: 15.4 MB (84.11%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,9 +1,10 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        L=len(nums)
         a=[-1,-1]
-        if len(nums)<=2:
+        if L<=2:
             if target in nums:
-                if len(nums)==2:
+                if L==2:
                     if nums[0]==target and nums[1]==target:
                         return [0,1]
                     if nums[0]==target and nums[1]!=target:
@@ -14,26 +15,26 @@ class Solution:
                         return [-1,-1]
                 else:
                     return [0,0]
-        if len(nums)==0:
+        if L==0:
             return [-1,-1]
         if nums[0]==target:
-            for i in range(len(nums)):
+            for i in range(L):
                 if nums[i]>target:
                     return [0,i-1]
-        for i in range(len(nums)):
+        for i in range(L):
             if nums[i]<target:
                 a[0]=i+1
             if nums[i]>target:
                 a[1]=i-1
                 break
         if nums[-1]==target and nums[-2]==target:
-            a[1]=len(nums)-1
+            a[1]=L-1
             if nums[0]==target:
                 a[0]=0
             return a
         
         if nums[-1]==target and nums[-2]!=target:
-            return [len(nums)-1,len(nums)-1]
+            return [L-1,L-1]
         if target not in nums:
             return [-1,-1]
         # if a[0]==nums[0] and a[1]==nums[0]:
