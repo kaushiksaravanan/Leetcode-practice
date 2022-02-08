e57f3638e47d9053b2c6940c9bed0b72a2ff72d6
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-02-08 11:20:53 +0530

    Time: 53 ms (16.94%), Space: 13.7 MB (99.59%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,14 @@
+t={}
+def tfib(n):
+    if n in t:
+        return t[n]
+    if n==0:
+        return 0
+    elif n==1 or n==2:
+        return 1
+    else:
+        t[n]=tfib(n-2)+tfib(n-1)+tfib(n-3)
+        return t[n]
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        return tfib(n)
