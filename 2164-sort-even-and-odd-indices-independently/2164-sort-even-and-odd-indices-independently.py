0093e536627fca0af61afce9cfdb586183d7e52e
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-02-26 12:48:36 +0530

    Time: 96 ms (36.15%), Space: 13.8 MB (75.62%) - LeetHub

---
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.py
@@ -0,0 +1,29 @@
+class Solution:
+    def sortEvenOdd(self, arr: List[int]) -> List[int]:
+        n=len(arr)
+        st=[]
+        en=[]
+        for i in range(n):
+            if i%2==0:
+                st.append(arr[i])
+            else:
+                en.append(arr[i])
+        st.sort()
+        en.sort()
+        e=[]
+        k=0
+        l=len(en)-1
+        for i in range(n):
+            if i%2==0:
+                e.append(st[k])
+                k+=1
+            else:
+                e.append(en[l])
+                l-=1
+        
+        
+
+        return e
+        
+            
+        
