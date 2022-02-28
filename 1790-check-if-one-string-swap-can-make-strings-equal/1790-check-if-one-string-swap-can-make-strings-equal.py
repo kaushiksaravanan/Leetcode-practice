a8c60ab1a1217008b39e2a38d3c68cc038c05e11
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-02-28 21:48:06 +0530

    Time: 58 ms (20.12%), Space: 13.7 MB (99.24%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -6,16 +6,12 @@ class Solution:
         for i in range(len(s1)):
             if s1[i]!=s2[i]:
                 k.append([s1[i],s2[i]])
-        if len(k)>2:
-            return False
-        if len(k)==1:
-            return False
+        if len(k)==0:
+            return True
         if len(k)==2:
             r1=k[0]
             r2=k[1]
             if r1[0]==r2[1] and r1[1]==r2[0]:
                 return True
-        if len(k)==0:
-            return True
         return False
                 
