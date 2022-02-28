6d0614c27d4d5d4c5ec95942b8d478214c35a91c
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-02-28 21:49:04 +0530

    Time: 36 ms (71.49%), Space: 14 MB (58.84%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -1,7 +1,5 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        if len(s1)!=len(s2):
-            return False
         k=[]
         for i in range(len(s1)):
             if s1[i]!=s2[i]:
@@ -9,9 +7,7 @@ class Solution:
         if len(k)==0:
             return True
         if len(k)==2:
-            r1=k[0]
-            r2=k[1]
-            if r1[0]==r2[1] and r1[1]==r2[0]:
+            if k[0][0]==k[1][1] and k[0][1]==k[1][0]:
                 return True
         return False
                 
