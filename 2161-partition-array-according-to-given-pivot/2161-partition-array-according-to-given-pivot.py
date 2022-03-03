9e7ef259bbbad39f07dc68b060664e3182e76501
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-03-03 23:30:17 +0530

    Time: 1657 ms (78.24%), Space: 31.7 MB (16.95%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        st=[]
+        en=[]
+        mi=[]
+        k=[]
+        for i in nums:
+            if i<pivot:
+                st.append(i)
+            if i>pivot:
+                en.append(i)
+            if i==pivot:
+                mi.append(i)
+                
+        return st+mi+en
