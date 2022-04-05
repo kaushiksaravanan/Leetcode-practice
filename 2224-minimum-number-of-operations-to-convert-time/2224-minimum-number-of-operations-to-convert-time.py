a622224dad1df4298403296508c46b74abd28a83
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-04-05 21:25:35 +0530

    Time: 47 ms (44.88%), Space: 14 MB (30.34%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
@@ -1,11 +1,7 @@
 class Solution:
     def convertTime(self, current: str, correct: str) -> int:
-        curr_hr=int(current[:2])
-        curr_min=int(current[-2:])
-        corr_hr=int(correct[:2])
-        corr_min=int(correct[-2:])
         cou_60=0
-        diff=corr_min-curr_min+60*(corr_hr-curr_hr)
+        diff=int(correct[-2:])-int(current[-2:])+60*(int(correct[:2])-int(current[:2]))
         while diff>0:
             y=diff//60
             diff-=y*60
