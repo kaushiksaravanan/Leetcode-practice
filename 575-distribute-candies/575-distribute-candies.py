f5e055cce0119ebb8190fc30ff0037bb736a0f3e
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-05-01 22:49:29 +0530

    Time: 1631 ms (5.02%), Space: 16.3 MB (40.36%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.py
+++ 575-distribute-candies/575-distribute-candies.py
@@ -1,13 +1,7 @@
-def fact(n):
-    if n>0:
-        return n*fact(n-1)
-    else:
-        return 1
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
         n=len(candyType)//2
         r=len(set(candyType))
         if n>r:
             return r
-        else:
-            return n
+        return n
