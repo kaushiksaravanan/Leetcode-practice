702b415a46e37eebaf099883cb8615ab452d19bf
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-05-27 15:07:02 +0530

    Time: 2741 ms (5.01%), Space: 23.6 MB (99.96%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,23 +1,19 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
         tot_sum=sum(nums)
-        print(tot_sum)
+        # print(tot_sum)
         n=len(nums)
-        min_k=9999999
+        min_k=999999
         ind=0
         prev_sum=nums[0]
         for i in range(n-1):
             fir_deno=i+1
             last_deno=n-fir_deno
-           
             m=abs(prev_sum//fir_deno-(tot_sum-prev_sum)//last_deno)
             print(m,i,ind)
             if m<min_k:
-                print('hhhhh',m,i,ind)
                 min_k=m
                 ind=i
-            # print(m,i,ind,'after')
-
             prev_sum+=nums[i+1]
         if min_k>tot_sum//n:
             return n-1
