b9169d65eb8c25d126e16183c7230c82aa8a57b4
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2022-07-31 00:19:08 +0530

    Time: 123 ms (19.48%), Space: 15.2 MB (45.98%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.py
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -1,25 +1,28 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         x=[]
-        def f(index,flag):
+        flag=[-2]*len(s)
+        def f(index):
             if index>=len(s):
                 l=0
                 ss=''
                 for i in flag:
                     if i==1:
                         ss+=s[l].lower()
-                    if i==0:
+                    elif i==0:
                         ss+=s[l].upper()
+                    elif i==-1:
+                        ss+=s[l]
                     l+=1
                 x.append(ss[:])
                 return
             if not s[index].isdigit():
                 flag[index]=1
-                f(index+1,flag)
+                f(index+1)
                 flag[index]=0
-                f(index+1,flag)
+                f(index+1)
             else:
-                flag[index]=1
-                f(index+1,flag)
-        f(0,[-1]*len(s))
+                flag[index]=-1
+                f(index+1)
+        f(0)
         return x
