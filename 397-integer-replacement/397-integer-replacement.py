8a8b7c3e944c2a7be93e1cb4f9789c5235dd06b0
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2022-07-30 18:18:13 +0530

    Time: 224 ms (32.07%), Space: 13.9 MB (67.64%) - LeetHub

---
+++ 397-integer-replacement/397-integer-replacement.py
@@ -0,0 +1,21 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        def f(n):
+            if n==1:
+                return 0
+            if n%2==0:
+                return 1+f(n//2)
+            else:
+                n1=1+f(n-1)
+                n2=1+f(n+1)
+                return min(n1,n2)
+        return f(n)
+        
+        '''Given a positive integer n, you can apply one of the following operations:
+
+If n is even, replace n with n / 2.
+If n is odd, replace n with either n + 1 or n - 1.
+Return the minimum number of operations needed for n to become 1.
+
+ '''
+        
