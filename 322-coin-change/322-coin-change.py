70ca61efc40921353e99efde39c6dad306fbb244
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-07-30 12:00:14 +0530

    Time: 4031 ms (6.30%), Space: 27.8 MB (16.18%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,28 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # d = [[9999 for i in range(amount+1000) for j in range(amount+1000)]]
+        n=len(coins)
+        # d = [[999999 for i in range(index+1)] for j in range(amount+1)]
+        # @lru_cache(None)
+        def f(index,amount,d):
+
+            if index==0:
+                if amount%coins[index]==0:
+                    return amount//coins[index]
+                else:
+                    return 999999
+            if (index,amount) in d:
+                return d[(index,amount)]
+            
+            n1=f(index-1,amount,d)
+            n2=999999
+            if coins[index]<=amount:
+                n2=1+f(index,amount-coins[index],d)
+            d[(index,amount)]=min(n1,n2)
+            return d[(index,amount)]
+        m=f(n-1,amount,{})
+        if m==999999:
+            return -1
+        return m
+            
+        
