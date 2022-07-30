81bc47a0f7472ca5747cb9d29b30a8437e36f3a9
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-07-30 12:23:32 +0530

    Time: 5263 ms (5.00%), Space: 28.9 MB (15.48%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,6 +1,6 @@
-# d={}
+d={}
 # @lru_cache(None)
-def f(index,amount,d,coins):
+def f(index,amount,coins):
 
             if index==0:
                 if amount%coins[index]==0:
@@ -10,19 +10,19 @@ def f(index,amount,d,coins):
             if (index,amount) in d:
                 return d[(index,amount)]
             
-            n1=f(index-1,amount,d,coins)
+            n1=f(index-1,amount,coins)
             n2=999999
             if coins[index]<=amount:
-                n2=1+f(index,amount-coins[index],d,coins)
+                n2=1+f(index,amount-coins[index],coins)
             d[(index,amount)]=min(n1,n2)
             return d[(index,amount)]
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        d={}
+        d.clear()
         # d = [[9999 for i in range(amount+1000) for j in range(amount+1000)]]
         n=len(coins)
         # d = [[999999 for i in range(index+1)] for j in range(amount+1)]
-        m=f(n-1,amount,d,coins)
+        m=f(n-1,amount,coins)
         if m==999999:
             return -1
         return m
