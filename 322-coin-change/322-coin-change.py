905eeeb29ebc8b981b75f589257dc75fa83621fb
Kaushik-Ss <75175772+Kaushik-Ss@users.noreply.github.com>
2022-07-30 12:20:09 +0530

    Time: 4700 ms (5.00%), Space: 28.8 MB (15.58%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,12 +1,6 @@
 # d={}
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        d={}
-        # d = [[9999 for i in range(amount+1000) for j in range(amount+1000)]]
-        n=len(coins)
-        # d = [[999999 for i in range(index+1)] for j in range(amount+1)]
-        @lru_cache(None)
-        def f(index,amount):
+# @lru_cache(None)
+def f(index,amount,d,coins):
 
             if index==0:
                 if amount%coins[index]==0:
@@ -16,13 +10,19 @@ class Solution:
             if (index,amount) in d:
                 return d[(index,amount)]
             
-            n1=f(index-1,amount)
+            n1=f(index-1,amount,d,coins)
             n2=999999
             if coins[index]<=amount:
-                n2=1+f(index,amount-coins[index])
+                n2=1+f(index,amount-coins[index],d,coins)
             d[(index,amount)]=min(n1,n2)
             return d[(index,amount)]
-        m=f(n-1,amount)
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        d={}
+        # d = [[9999 for i in range(amount+1000) for j in range(amount+1000)]]
+        n=len(coins)
+        # d = [[999999 for i in range(index+1)] for j in range(amount+1)]
+        m=f(n-1,amount,d,coins)
         if m==999999:
             return -1
         return m
