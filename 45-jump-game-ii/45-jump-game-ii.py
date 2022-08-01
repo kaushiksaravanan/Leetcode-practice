96c89dc4b27f9509cd57ca63323144fd7905c554
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2022-08-02 01:57:36 +0530

    Time: 12937 ms (9.27%), Space: 33.8 MB (5.02%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,16 +1,15 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         d={}
+        n=len(nums)
         @lru_cache(None)
         def f(index):
-            if index>=len(nums)-1:
+            if index>=n-1:
                 return 0
             k=999999
             if index in d:
                 return d[index]
             for i in range(1,nums[index]+1):
-                # if i<len(nums):
-                    # if index+i!=index:
                         n1=1+f(index+i)
                         k=min(k,n1)
             d[index]=k
