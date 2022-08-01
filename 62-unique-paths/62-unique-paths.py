e2617213433ffab55795b883d1d31e83e30e85e5
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2022-08-02 00:48:45 +0530

    Time: 74 ms (5.37%), Space: 14.3 MB (6.77%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,5 +1,14 @@
 import math
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-       return math.factorial(m+n-2)//(math.factorial(n-1)*math.factorial(m-1))
-        
+        d={}
+        def f(r,c):
+            if c==n-1 or r==m-1:
+                return 1
+            if (r,c) in d:
+                return d[(r,c)]
+            n1=f(r,c+1)
+            n2=f(r+1,c)
+            d[(r,c)]=n1+n2
+            return d[(r,c)]
+        return f(0,0)
