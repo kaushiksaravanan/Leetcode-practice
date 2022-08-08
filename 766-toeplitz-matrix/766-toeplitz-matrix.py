6424d190b9c0fac1f0bd23ba300654eb3d8f565d
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2022-08-08 23:03:32 +0530

    Time: 141 ms (44.90%), Space: 14 MB (37.27%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.py
+++ 766-toeplitz-matrix/766-toeplitz-matrix.py
@@ -8,8 +8,9 @@ class Solution:
                 if (i,j) not in visited:
                     for x in range(1,n):
                         if i+x<n and x+j<m:
-                            visited.add((i+x,x+j))
-                            if matrix[i][j]!=matrix[i+x][x+j]:
-                                return False
+                            if (x+i,x+j) not in visited:
+                                visited.add((i+x,x+j))
+                                if matrix[i][j]!=matrix[i+x][x+j]:
+                                    return False
         return True
         
