d98cfc67c9781c78f9132ec0947dd6f0692859f2
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2022-08-19 01:47:41 +0530

    Time: 44 ms (91.47%), Space: 17.2 MB (5.01%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -2,17 +2,21 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s3)!=len(s1)+len(s2):
             return False
+        d={}
         @cache
         def recur(i,j):
             if i==len(s1) and j==len(s2):
                 return True
+            if (i,j) in d:
+                return d[(i,j)]
             c1=False
             c2=False
             if i<len(s1) and s1[i]==s3[i+j]:
                 c1=recur(i+1,j)
             if j<len(s2) and s2[j]==s3[i+j]:
                 c2=recur(i,j+1)
-            return c1 or c2
+            d[(i,j)]=c1 or c2
+            return d[(i,j)]
         # print('jj')
         return recur(0,0)
         
