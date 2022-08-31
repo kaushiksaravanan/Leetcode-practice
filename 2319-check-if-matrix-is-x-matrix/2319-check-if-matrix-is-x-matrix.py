16f8553c001bd028b595890951297d8815a2560a
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2022-08-31 15:18:42 +0530

    Time: 417 ms (55.91%), Space: 15.1 MB (19.27%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
@@ -3,12 +3,18 @@ class Solution:
         n=len(grid)
         for i in range(n):
             for j in range(n):
-                if grid[i][j]==0:
-                    if i==j:
+                if (i+j==n-1 or i==j):
+                    if grid[i][j]==0:
                         return False
                 else:
-                    if i+j!=n-1 and i!=j:
+                    if grid[i][j]!=0:
                         return False
-                if i+j==n-1 and grid[i][j]==0:
-                    return False
+                # if grid[i][j]==0:
+                #     if i==j:
+                #         return False
+                # else:
+                #     if i+j!=n-1 and i!=j:
+                #         return False
+                # if i+j==n-1 and grid[i][j]==0:
+                    # return False
         return True
