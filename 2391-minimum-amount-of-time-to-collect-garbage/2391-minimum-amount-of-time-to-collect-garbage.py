47894270fb8463a0100e549d76e47c94f5a29055
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2022-09-03 00:41:10 +0530

    Time: 2129 ms (27.46%), Space: 36 MB (97.65%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -1,32 +1,9 @@
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
         cost=0
-        # for i in garbage:
-        #     cost=i.count('G')
-        #     travel
-        # for i in ['G','P','M']:
-        # l=-1
         pre=[0]
         for i in travel:
                 pre.append(pre[-1]+i)
-        
-        # for let in ['G','P','M']:
-        #     xx=0
-        #     last=0
-        #     for i in garbage:
-        #         n1=i.count(let)
-        #         if n1!=0:
-        #             last=xx
-        #             cost+=n1
-        #         xx+=1
-            # print(xx)
-            # if last==len(pre):
-                # cost+=pre[last-1]
-            # else:
-            # cost+=pre[last]
-            # if l!=-1:
-            # l+=1
-        # print(pre)
         i=0
         n1_last=0
         n2_last=0
@@ -42,25 +19,7 @@ class Solution:
                 n2_last=i
             if n3!=0:
                 n3_last=i
-            
-            # print(n1,n2,n3,garb,cost)
-            cost+=(n1+n2+n3)
+            cost=n1+n2+n3+cost
             i+=1
-        print(cost)
         return cost+pre[n1_last]+pre[n2_last]+pre[n3_last]
-        #     if l!=-1:
-        #         cost+=travel[l]*(n1+n2+n3)
-        #     l+=1
-            # l=-1
-            # for garb in garbage:
-            #     # if garb!='':
-            #         cost+=garb.count(i)
-            #         cost+=travel[l]
-            #     if l!=-1:
-            #         cost+=travel[l]
-            #     l+=1
-            #     # garb=garb.replace(i,'')
-        # print(garbage)
-        # for 
-        return cost
         
