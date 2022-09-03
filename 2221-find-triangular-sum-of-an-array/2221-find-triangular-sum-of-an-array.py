8c7fa0fb98a0eab593d5bb22139dff501053d08a
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2022-09-03 11:00:22 +0530

    Time: 3690 ms (61.44%), Space: 19 MB (12.40%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
@@ -3,9 +3,9 @@ class Solution:
         def ch(arr):
             if len(arr)<=2:
                 return sum(arr)
-            k=[]
+            k=[0]*(len(arr)-1)
             for i in range(0,len(arr)-1):
-                k.append((arr[i]+arr[i+1])%10)
+                k[i]=((arr[i]+arr[i+1])%10)
             return ch(k)
         return ch(nums)%10
         
