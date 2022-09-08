f6a38a9f0df2ea4f12e67cee5987939cde1d87bd
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2022-09-09 00:28:55 +0530

    Time: 42 ms (95.88%), Space: 13.8 MB (98.54%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.py
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.py
@@ -1,8 +1,8 @@
 class Solution:
     def checkDistances(self, s: str, distance: List[int]) -> bool:
         d={}
-        for i,v in enumerate(s):
-            d[v]=i
+        for i in range(len(s)):
+            d[s[i]]=i
         for i in range(len(s)):
             if d[s[i]]!=i:
                 if d[s[i]]-i-1!=distance[ord(s[i])-97]:
