ff7ca0c62f70ad8731e9a161546239a7180c4542
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2022-10-31 21:45:38 +0530

    Time: 158 ms (68.58%), Space: 13.8 MB (78.65%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        d={}
+        n=len(matrix)
+        m=len(matrix[0])
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] not in d:
+                    for ii in range(n+1000):
+                        try:
+                            if matrix[i][j]!=matrix[i+ii][j+ii]:
+                                return False
+                        except:
+                            break
+        return True
+                            
+                    # d[matrix[i][j]]=1
+                    
