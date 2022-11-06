53a4e38590e571349cd110030692e8541e3d456e
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2022-11-06 13:19:05 +0530

    Time: 86 ms (5.56%), Space: 14.2 MB (22.87%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        n=len(s)
+        d={}
+        for i in s:
+            if i in d:
+                d[i]+=1
+            if i not in d:
+                d[i]=1
+        l=0
+        flag=0
+        odds=[]
+        evens=[]
+        for i in d:
+            print(i,d[i])
+            if d[i]%2==0:
+                evens.append(d[i])
+            if d[i]%2!=0:
+                odds.append(d[i])
+        odds.sort()
+        odds=odds[::-1]
+        try:
+            l=odds[0]
+        except:
+            pass
+        for i in odds[1:]:
+            print(i)
+            l+=(i-1)
+        return sum(evens)+l
+        
